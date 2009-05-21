f398ad7d8f234aefa9cd0d201444a656ed9354cf
Min Kim <minho.kim@gmail.com>
2009-05-21 15:11:00 -0400

    adding service endpoint for retreiving movie times for a given zip code

--- app/controllers/theaters_controller.rb
+++ app/controllers/theaters_controller.rb
@@ -1,8 +1,7 @@
 class TheatersController < ApplicationController
   
   def index
-    
-    
+    @theaters = Theater.showtimes(params[:postal_code])
   end
   
 end
--- app/models/theater.rb
+++ app/models/theater.rb
@@ -1,7 +1,21 @@
 class Theater < ActiveRecord::Base
   
-  def self.showtimes(postal_code)
-    theaters = HTTParty.get("http://new.api.movies.yahoo.com/v2/listTheatersByPostalCode?pcode=#{postal_code}&count=100&yprop=msapi")
-  end
+  attr_accessor :showtimes
   
+  def self.showtimes(postal_code)
+    response = HTTParty.get("http://new.api.movies.yahoo.com/v2/listTheatersByPostalCode?pcode=#{postal_code}&count=100&yprop=msapi")
+    puts response.inspect;
+    yids = response['TheaterList']['Theater'].map{|t| t['theater:id'].to_i}
+    theaters = Theater.all(:conditions => {:yid => yids})
+    
+    theaters.each do |theater|
+      data = response['TheaterList']['Theater'].detect{|t| t['theater:id'].to_i == theater.yid }
+      
+      theater.showtimes = 
+        data['MovieList']['Movie'].map do |m| 
+          { m['movie:Title'] => m['Shows']['Time'].map{|t| Time.parse(t).strftime('%I:%M') }}
+        end rescue []
+    end
+    theaters
+  end  
 end
