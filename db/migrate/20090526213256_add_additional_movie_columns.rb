56f460f890f313fa96a3efd24dbb434ba952753f
Min Kim <minho.kim@gmail.com>
2009-05-26 17:51:50 -0400

    duration column already exists

--- db/migrate/20090526213256_add_additional_movie_columns.rb
+++ db/migrate/20090526213256_add_additional_movie_columns.rb
@@ -1,13 +1,11 @@
 class AddAdditionalMovieColumns < ActiveRecord::Migration
   def self.up
     add_column :movies, :released_at, :datetime
-    add_column :movies, :duration, :integer
     add_column :movies, :gross, :integer
   end
 
   def self.down
     remove_column :movies, :released_at
-    remove_column :movies, :duration
     remove_column :movies, :gross
   end
 end
