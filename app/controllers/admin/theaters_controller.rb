59568bd991f44ea3e5cd619580b4737e22559503
Min Kim <minho.kim@gmail.com>
2009-05-28 17:39:31 -0400

    adding updates to theaters

--- app/controllers/admin/theaters_controller.rb
+++ app/controllers/admin/theaters_controller.rb
@@ -5,4 +5,22 @@ class Admin::TheatersController < Admin::BaseController
     @theaters = Theater.paginate(:page => @page, :order => 'name desc', :per_page => 20)
   end
   
+  def edit
+    @theater = Theater.find params[:id]
+  end
+  
+  def update
+    @theater = Theater.find params[:id]
+
+    if @theater.update_attributes(params[:theater])
+      flash[:success] = "Theater saved!" if flash[:success].blank?
+      redirect_to edit_admin_theater_path(@theater)
+      return false
+    end
+    
+    flash.now[:error] = "Error updating theater!" if flash.now[:error].blank?
+    
+    render :action => 'edit'
+  end
+  
 end
